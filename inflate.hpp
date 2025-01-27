1cf218dd7b13eb53534d0e80947a2ab632734e05
game-programmer-pc <klugasteven25565@gmail.com>
2025-01-27 09:57:39 -0600

    Update inflate.hpp

--- inflate.hpp
+++ inflate.hpp
@@ -4,20 +4,20 @@
 #include <cstdint>
 #include <vector>
 
-#define INFLATE_OK								((0 << 31) | (1 << 29) | (300 << 16) | (0))
-#define INFLATE_FALSE							((0 << 31) | (1 << 29) | (300 << 16) | (1))
-#define INFLATE_MEMORY_ACCESS_FAIL				((1 << 31) | (1 << 29) | (300 << 16) | (0))
-#define INFLATE_SYMBOL_NOT_FOUND				((1 << 31) | (1 << 29) | (300 << 16) | (1))
-#define INFLATE_INVALID_DISTANCE				((1 << 31) | (1 << 29) | (300 << 16) | (2))
-#define INFLATE_INVALID_LENGTH					((1 << 31) | (1 << 29) | (300 << 16) | (3))
-#define INFLATE_INVALID_DISTANCE_SYMBOL			((1 << 31) | (1 << 29) | (300 << 16) | (4))
-#define INFLATE_FINAL_BLOCK_MISPLACED			((1 << 31) | (1 << 29) | (300 << 16) | (5))
-#define INFLATE_DATA_INTEGRITY_FAIL				((1 << 31) | (1 << 29) | (300 << 16) | (6))
+#define INFLATE_OK				((0 << 31) | (1 << 29) | (300 << 16) | (0))
+#define INFLATE_FALSE				((0 << 31) | (1 << 29) | (300 << 16) | (1))
+#define INFLATE_MEMORY_ACCESS_FAIL		((1 << 31) | (1 << 29) | (300 << 16) | (0))
+#define INFLATE_SYMBOL_NOT_FOUND		((1 << 31) | (1 << 29) | (300 << 16) | (1))
+#define INFLATE_INVALID_DISTANCE		((1 << 31) | (1 << 29) | (300 << 16) | (2))
+#define INFLATE_INVALID_LENGTH			((1 << 31) | (1 << 29) | (300 << 16) | (3))
+#define INFLATE_INVALID_DISTANCE_SYMBOL		((1 << 31) | (1 << 29) | (300 << 16) | (4))
+#define INFLATE_FINAL_BLOCK_MISPLACED		((1 << 31) | (1 << 29) | (300 << 16) | (5))
+#define INFLATE_DATA_INTEGRITY_FAIL		((1 << 31) | (1 << 29) | (300 << 16) | (6))
 #define INFLATE_UNCOMPRESSED_LENGTH_MISMATCH	((1 << 31) | (1 << 29) | (300 << 16) | (7))
-#define INFLATE_UNKNOWN_SYMBOL					((1 << 31) | (1 << 29) | (300 << 16) | (8))
-#define INFLATE_REPEAT_CODE_FAIL				((1 << 31) | (1 << 29) | (300 << 16) | (9))
-#define INFLATE_INVALID_WINDOW_SIZE				((1 << 31) | (1 << 29) | (300 << 16) | (10))
-#define INFLATE_INVALID_BLOCK					((1 << 31) | (1 << 29) | (300 << 16) | (11))
+#define INFLATE_UNKNOWN_SYMBOL			((1 << 31) | (1 << 29) | (300 << 16) | (8))
+#define INFLATE_REPEAT_CODE_FAIL		((1 << 31) | (1 << 29) | (300 << 16) | (9))
+#define INFLATE_INVALID_WINDOW_SIZE		((1 << 31) | (1 << 29) | (300 << 16) | (10))
+#define INFLATE_INVALID_BLOCK			((1 << 31) | (1 << 29) | (300 << 16) | (11))
 
 
 std::int32_t
